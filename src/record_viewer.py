e844f1ba7a951be2e1d1b61f1e57b931517f1395
Shawn Armstrong <smarmstr@ucsc.edu>
2023-09-05 21:20:31 -0700

    Resolve table dimensions issues.

--- src/record_viewer.py
+++ src/record_viewer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.font as tkFont
 
 class RecordApp:
     def __init__(self, master, records):
@@ -44,6 +45,7 @@ class RecordApp:
         self.master.grid_columnconfigure(0, weight=1)
         self.setup_notebook_tabs()
         self.setup_endpoint_section()
+        self.setup_production_page_section()
         
     def setup_notebook_tabs(self):
         self.notebook = ttk.Notebook(self.master)
@@ -66,57 +68,51 @@ class RecordApp:
 
         # Create frame to contain the endpoint contents
         self.endpoint_label = ttk.LabelFrame(self.main_frame, text="Endpoints", padding=(10, 10), width=300, height=700)
-        self.endpoint_label.grid(row=0, column=0, pady=10, padx=10, sticky='nsew')
+        self.endpoint_label.grid(row=0, column=0, rowspan=2, pady=10, padx=10, sticky='nsew')
 
-        # Configure the frame grid to ensure its children widgets consume entire frame
+        # Configure the frame grid to allow its children  consume entire frame
         self.endpoint_label.grid_rowconfigure(0, weight=1)  
         self.endpoint_label.grid_columnconfigure(0, weight=1) 
         
-        # Prevent the label frame itself from resizing
+        # Prevent the frame itself from resizing
         self.endpoint_label.grid_propagate(flag=False)
 
         # Create a vertical scrollbar inside the frame
         self.endpoint_select_vsb = ttk.Scrollbar(self.endpoint_label, orient="vertical")
         self.endpoint_select_vsb.grid(row=0, column=1, sticky='ns')
 
-        # Create a listbox inside the frame to display the endpoints. 
+        # Create a listbox inside the frame to display the endpoints
         self.endpoint_listbox = tk.Listbox(self.endpoint_label, selectmode=tk.SINGLE, yscrollcommand=self.endpoint_select_vsb.set)
         self.endpoint_listbox.grid(row=0, column=0, sticky='nsew')
 
-        # Configure the vertical scrollbar to control the scrolling of the listbox.
+        # Configure the vertical scrollbar to the listbox
         self.endpoint_select_vsb.config(command=self.endpoint_listbox.yview)
 
         # Populate the listbox with endpoint records.
         for _, record in self.records.items():
             self.endpoint_listbox.insert(tk.END, record.endpoint)
 
-        # # TO DO LATER
-        # self.endpoint_listbox.bind('<<ListboxSelect>>', self.on_endpoint_selected)
-
+        self.endpoint_listbox.bind('<<ListboxSelect>>', self.on_endpoint_selected)
 
     def setup_production_page_section(self):
-        # Create a labeled frame titled 'Production Page' in the main frame. This acts as a separator or section for production page content.
-        self.production_page_separator = ttk.LabelFrame(self.main_frame, text="Production Page", padding=(20, 10))
-        self.production_page_separator.grid(row=0, column=1, pady=20, padx=20, sticky='nsew')
 
-        self.main_frame.grid_rowconfigure(0, weight=1)
-        self.main_frame.grid_columnconfigure(0, weight=1)
+        self.production_page_label = ttk.LabelFrame(self.main_frame, text="Production Page", padding=(10, 10), width=1000, height=330)
+        self.production_page_label.grid(row=0, column=1, pady=10, padx=10, sticky='n')
+        
+        # Allow children frames to expand horizontally
+        self.production_page_label.grid_columnconfigure(0, weight=1)
 
-        # Create a frame inside the labeled frame for displaying logs.
-        self.production_page_frame = ttk.Frame(self.production_page_separator)
-        self.production_page_frame.pack(pady=20, padx=20, fill=tk.BOTH, expand=True)
-
-        self.production_page_separator.grid_rowconfigure(0, weight=1)
-        self.production_page_separator.grid_columnconfigure(0, weight=1)
+        # Prevent the frame itself from resizing
+        self.production_page_label.grid_propagate(flag=False)
 
         # Initialize the treeview with specified columns and configure it to show column headings.
-        self.production_console_tree = ttk.Treeview(self.production_page_frame, 
+        self.production_console_tree = ttk.Treeview(self.production_page_label, 
                                         columns=("Type", "Level", "Source", "Message"), 
-                                        show="headings", height=10)
+                                        show="headings", height=4)
         
         # Create vertical and horizontal scrollbars for the treeview.
-        vsb = ttk.Scrollbar(self.production_page_frame, orient="vertical", command=self.production_console_tree.yview)
-        hsb = ttk.Scrollbar(self.production_page_frame, orient="horizontal", command=self.production_console_tree.xview)
+        vsb = ttk.Scrollbar(self.production_page_label, orient="vertical", command=self.production_console_tree.yview)
+        hsb = ttk.Scrollbar(self.production_page_label, orient="horizontal", command=self.production_console_tree.xview)
 
         # Connect the scrollbars to the treeview.
         self.production_console_tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
@@ -125,26 +121,22 @@ class RecordApp:
         self.production_console_tree.tag_configure('evenrow', background='#FFFFFF')  
         self.production_console_tree.tag_configure('oddrow', background='#d3d3d3') 
 
-        # Grid the treeview and scrollbars within the production_page_frame.
+        # Grid the treeview and scrollbars within the production_page_label.
         self.production_console_tree.grid(row=0, column=0, sticky='nsew')
         vsb.grid(row=0, column=1, sticky='ns')
         hsb.grid(row=1, column=0, sticky='ew')
 
-        # Ensure the treeview expands properly when the production_page_frame is resized.
-        self.production_page_frame.grid_columnconfigure(0, weight=1)
-        self.production_page_frame.grid_rowconfigure(0, weight=1)
-
         # Define the width for each column in the treeview.
-        self.production_console_tree.column("Type", width=100)
-        self.production_console_tree.column("Level", width=100)
+        self.production_console_tree.column("Type", width=60)
+        self.production_console_tree.column("Level", width=50)
         self.production_console_tree.column("Source", width=100)
-        self.production_console_tree.column("Message", width=300)
+        self.production_console_tree.column("Message", width=2000)
 
         # Set the headings for each column in the treeview.
-        self.production_console_tree.heading("Type", text="Type")
-        self.production_console_tree.heading("Level", text="Level")
-        self.production_console_tree.heading("Source", text="Source")
-        self.production_console_tree.heading("Message", text="Message")
+        self.production_console_tree.heading("Type", text=" Type", anchor="w")
+        self.production_console_tree.heading("Level", text=" Level", anchor="w")
+        self.production_console_tree.heading("Source", text=" Source", anchor="w")
+        self.production_console_tree.heading("Message", text=" Message", anchor="w")
 
     def on_endpoint_selected(self, event):
         # First, clear any previous entries in the treeview
@@ -163,15 +155,13 @@ class RecordApp:
                 selected_record = record
                 break
         
-
-        
         if selected_record:  # Ensure that a matching record was found
             # Insert console_logs into the treeview
             for i, log in enumerate(selected_record.prod_logs['console_logs']):
 
                 row_tag = 'evenrow' if i % 2 == 0 else 'oddrow'
                 self.production_console_tree.insert('', 'end', values=('Console', log['level'], log['source'], log['message']), tags=(row_tag,))
-
+        
 
 def main():
     root = tk.Tk()
